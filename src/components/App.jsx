0dfe85700a9162bb12dd298d98cb959fa6016614
irynahamoum <111897707+irynahamoum@users.noreply.github.com>
2024-03-04 13:30:35 +0100

    update

---
+++ src/components/App.jsx
@@ -0,0 +1,20 @@
+// import { useState } from 'react';
+// import reactLogo from './assets/react.svg';
+// import viteLogo from '../../../../../vite.svg';
+import './App.css';
+import Profile from './Profile';
+import userData from '../userData.json';
+
+export default function App() {
+  return (
+    <>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+    </>
+  );
+}
---
+++ src/components/Profile.jsx
@@ -0,0 +1,39 @@
+import PropTypes from 'prop-types';
+
+const Profile = ({ name, tag, location, image, stats }) => {
+  return (
+    <div>
+      <div>
+        <img src={image} alt={name} />
+        <p>{name}</p>
+        <p>@{tag}</p>
+        <p>{location}</p>
+      </div>
+
+      <ul>
+        <li>
+          <span>Followers</span>
+          <span>{stats.followers}</span>
+        </li>
+        <li>
+          <span>Views</span>
+          <span>{stats.views}</span>
+        </li>
+        <li>
+          <span>Likes</span>
+          <span>{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+Profile.propTypes = {
+  name: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  stats: PropTypes.object.isRequired,
+};
+
+export default Profile;
